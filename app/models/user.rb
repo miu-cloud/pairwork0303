33134d97f0a25cfcebb656ff2616417267cc09e4
miu oohira <miu.000408@gmail.com>
2021-03-05 21:43:12 +0900

    お気に入り機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     has_secure_password  
-    validates :password, presence: true, length: { minimum: 6 }                
+    validates :password, presence: true, length: { minimum: 6 }    
+    has_many :blogs 
+    has_many :favorites, dependent: :destroy           
 end
